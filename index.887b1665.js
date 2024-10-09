44a545fd8d9dca99b843f076416f30f1286b5fe4
Igor <4430704@gmail.com>
2024-10-09 11:14:40 +0300

    make build

---
+++ index.887b1665.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("body"),n=document.createElement("div");n.id="tree",e.append(n),function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");c.textContent=a,r.append(c),Object.keys(t[a]).length>0&&e(c,t[a])}}(n,{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.887b1665.js.map
